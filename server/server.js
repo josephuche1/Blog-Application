08eec59bc8956b414067a3d126147ba62deeb682
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-04 15:01:21 +0200

    Configure user authentication using facebook and user credentials

--- server/server.js
+++ server/server.js
@@ -44,8 +44,10 @@ const postSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
    email:String,
+   username: String,
    password:String,
    googleId:String,
+   facebook:String,
    posts:[String], // Store user posts in an array with the post id
    profilepic:String
 });
@@ -64,15 +66,77 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new FacebookStrategy({
   clientID: process.env.APP_ID,
   clientSecret: process.env.APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/home"
+  callbackURL: "http://localhost:3000/auth/facebook/home",
+  profileFields: ["id", "displayName", "photos", "email"]
 }, 
 (accesstoken, refreshToken, profile, cb) => {
-   User.findOrCreate({facebook:profile.id}, (err, user) => {
+   User.findOrCreate({facebook:profile.id, email:profile.email, username: profile.email}, (err, user) => {
      return cb(err,user);
    })
 }
 ));
 
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get("/auth/facebook/home", 
+  passport.authenticate("facebook", { failureRedirect: "Login"}),
+  (req,res) => {
+    // successfully authenticated, redirect home
+    res.redirect("/");
+  });
+
+app.post("/register", async (req,res) => {
+   try{
+     //Searching for users with the same username, trying to create users with the unique username
+     const user = await User.findOne(({username: req.fields.email})); 
+     if(user){
+      console.log("username already taken.")
+      res.redirect("/register");
+     }
+     if(req.fields.password  === req.fields.confirmPassword){
+      User.register({username: req.body.email, email: req.fields.email, posts:[],profilepic:"default"}, req.fields.password, (err, user) => {
+        if(err){
+          console.log(`An error occurred while signing up: ${err.message}`);
+          res.redirect("/");
+        }
+        else{
+          passport.authenticate("local")(req, res, () => {
+              res.redirect(`/${req.body.username}`);
+          })
+        }
+      })
+     } else{
+      console.log("Password and Confirm PAssword must be the exact same.");
+      res.redirect("/register");
+     }
+   } catch(err) {
+     console.log(`An error occured: ${err.message}`);
+     res.status(500).send("An error in the server. please try again later.");
+   }
+});
+
+app.post("/login", (req, res) => {
+  try{
+    const user = new user({
+      username: req.body.email,
+      password:req.body.password, 
+    });
+    req.login(user, (err) => {
+      if(err){
+        console.log(`An error occured while tryingto log in: ${err.message}`);
+        res.redirect("/login");
+      } else{
+        passport.authenticate("local", {
+          failureRedirect: "/login",
+          successRedirect:"/"
+        })
+      }
+    })
+  } catch(err){
+     console.log(`An error occured: ${err.message}`);
+     res.status(500).send("An error has occured. Please try again later");
+  }
+});
 
 
 app.get('/api', (req, res) => {
@@ -80,5 +144,5 @@ app.get('/api', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`API server is running  on port ${port}`)
+    console.log(`API server is running on port ${port}`)
 });
