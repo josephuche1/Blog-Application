47f7089e062d1964a28470a9cb63fc24b61b3540
PARSIVALJ <uchejesusboy@gmail.com>
2023-12-19 18:50:54 +0200

    Set up for parsing user Post I will be using express-formidable to parse user posts Install express-formidable using npm Apply the middleware to specific routes to prevent clash between express-formidable and body-parser

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ import path from "path";
 import fs from "fs";
 import crypto from "crypto";
 import bodyParser from "body-parser";
+import Formidable from "express-formidable";
 
 const app = express();
 const port = 5000;
@@ -150,174 +151,174 @@ app.post("/register", (req, res) => {
 });
 
 
-// app.post("/login", (req, res) => {
-//   try{
-//     const user = new user({
-//       username: req.body.username,
-//       password:req.body.password, 
-//     });
-//     req.login(user, (err) => {
-//       if(err){
-//         res.json({isAuthenticated: false, error:err.message});
-//       } else{
-//         passport.authenticate("local", () => {
-//           res.json({isAuthenticated: true, user:user});
-//         })
-//       }
-//     })
-//   } catch(err){
-//     message =`An error occured: ${err.message}`
-//     res.redirect("/login");
-//   }
-// });
+app.post("/login", (req, res) => {
+  try{
+    const user = new user({
+      username: req.body.username,
+      password:req.body.password, 
+    });
+    req.login(user, (err) => {
+      if(err){
+        res.json({isAuthenticated: false, error:err.message});
+      } else{
+        passport.authenticate("local", () => {
+          res.json({isAuthenticated: true, user:user});
+        })
+      }
+    })
+  } catch(err){
+    message =`An error occured: ${err.message}`
+    res.redirect("/login");
+  }
+});
 
-// // Blog post API
-// app.get("/api/posts/:id", async (req,res) => {
-//   if(req.isAuthenticated()){
-//     const post = await Post.findById(req.params.id);
-//     if(post){
-//       res.json({post});
-//     } else{
-//       res.json({message: `Post ${req.params.id} not found`});
-//     }
-//   } else{
-//     res.json({isAuthenticated: false});
-//   }
-// });
+// Blog post API
+app.get("/api/posts/:id", async (req,res) => {
+  if(req.isAuthenticated()){
+    const post = await Post.findById(req.params.id);
+    if(post){
+      res.json({post});
+    } else{
+      res.json({message: `Post ${req.params.id} not found`});
+    }
+  } else{
+    res.json({isAuthenticated: false});
+  }
+});
 
-// app.post("/api/posts", async (req,res) => {
-//     try{
-//       if(req.files.image.name != ""){
-//         const image = req.files.image;
-//         const buf = crypto.randomBytes(16);
-//         const filePath = buf.toString("hex")+path.extname(image.name);
+app.post("/api/posts", Formidable(), async (req,res) => {
+    try{
+      if(req.files.image.name != ""){
+        const image = req.files.image;
+        const buf = crypto.randomBytes(16);
+        const filePath = buf.toString("hex")+path.extname(image.name);
 
-//         const readStream = fs.createReadStream(image.path);
+        const readStream = fs.createReadStream(image.path);
 
-//         const uploadStream = gfs.openUploadStream(filePath, {
-//           chunkSizeBytes:1048576,
-//           metadata:{
-//             name: image.name,
-//             size:image.size,
-//             type: image.type
-//           }
-//         });
-//         readStream.pipe(uploadStream);
-//         uploadStream.on("finish", async () => {
-//           const newPost = new Post({
-//             image:filePath,
-//             text: req.fields.text,
-//             likes: 0,
-//             comments:[],
-//             timestamp:new Date(),
-//             author:req.fields.author
-//           })
-//           await newPost.save();
-//           res.json(newPost);
-//         })
-//       } else{
-//         const newPost = new Post({
-//           text: req.fields.text,
-//           likes: 0,
-//           comments:[],
-//           timestamp:new Date()
-//         })
-//         await newPost.save();
-//         res.json(newPost);
-//       }
-//     } catch(err){
-//        res.json({message: `An error has occured: ${err.messsage}`});
-//     }
-// });
+        const uploadStream = gfs.openUploadStream(filePath, {
+          chunkSizeBytes:1048576,
+          metadata:{
+            name: image.name,
+            size:image.size,
+            type: image.type
+          }
+        });
+        readStream.pipe(uploadStream);
+        uploadStream.on("finish", async () => {
+          const newPost = new Post({
+            image:filePath,
+            text: req.fields.text,
+            likes: 0,
+            comments:[],
+            timestamp:new Date(),
+            author:req.fields.author
+          })
+          await newPost.save();
+          res.json(newPost);
+        })
+      } else{
+        const newPost = new Post({
+          text: req.fields.text,
+          likes: 0,
+          comments:[],
+          timestamp:new Date()
+        })
+        await newPost.save();
+        res.json(newPost);
+      }
+    } catch(err){
+       res.json({message: `An error has occured: ${err.messsage}`});
+    }
+});
 
-// app.get("/api/posts", async (req,res) => {
-//    try{
-//     const posts = await Post.find({});
-//     if(posts.length !== 0){
-//        res.json(posts);
-//     }
-//     else{
-//       res.json({message: "No posts yet"});
-//     }
-//    } catch(err){
-//      res.status(500).json({error: err.message});
-//    }
+app.get("/api/posts", async (req,res) => {
+   try{
+    const posts = await Post.find({});
+    if(posts.length !== 0){
+       res.json(posts);
+    }
+    else{
+      res.json({message: "No posts yet"});
+    }
+   } catch(err){
+     res.status(500).json({error: err.message});
+   }
    
-// });
+});
 
-// app.patch("/api/posts/:id", async (req,res) => {
-//   const post = await Post.findById(req.params.id);
-//   try{
-//     if(req.files.image.name != ""){
-//       if(post.image != "" || post.image){
-//         const old =gfs.find({filename: post.image}).toArray();
-//         if(post.length != 0){
-//           await gfs.delete(old[0]._id);
-//         }
-//       }
-//       const image = req.files.image;
-//       const buf = crypto.randomBytes(16);
-//       const filePath = buf.toString("hex")+path.extname(image.name);
+app.patch("/api/posts/:id", Formidabble(), async (req,res) => {
+  const post = await Post.findById(req.params.id);
+  try{
+    if(req.files.image.name != ""){
+      if(post.image != "" || post.image){
+        const old =gfs.find({filename: post.image}).toArray();
+        if(post.length != 0){
+          await gfs.delete(old[0]._id);
+        }
+      }
+      const image = req.files.image;
+      const buf = crypto.randomBytes(16);
+      const filePath = buf.toString("hex")+path.extname(image.name);
 
-//       const readStream = fs.createReadStream(image.path);
+      const readStream = fs.createReadStream(image.path);
 
-//       const uploadStream = gfs.openUploadStream(filePath, {
-//         chunkSizeBytes:1048576,
-//         metadata:{
-//           name: image.name,
-//           size:image.size,
-//           type: image.type
-//         }
-//       });
-//       readStream.pipe(uploadStream);
-//       uploadStream.on("finish",async () => {
-//         post.image = filePath;
-//         await post.save();
-//       })
-//     } 
-//     post.text = post.text || req.fields.text;
-//     post.timestamp = new Date();
-//     await post.save();
-//     res.json(post);
+      const uploadStream = gfs.openUploadStream(filePath, {
+        chunkSizeBytes:1048576,
+        metadata:{
+          name: image.name,
+          size:image.size,
+          type: image.type
+        }
+      });
+      readStream.pipe(uploadStream);
+      uploadStream.on("finish",async () => {
+        post.image = filePath;
+        await post.save();
+      })
+    } 
+    post.text = post.text || req.fields.text;
+    post.timestamp = new Date();
+    await post.save();
+    res.json(post);
     
-//   } catch(err){
-//      res.json({message: `An error has occured: ${err.messsage}`});
-//   }
-// });
+  } catch(err){
+     res.json({message: `An error has occured: ${err.messsage}`});
+  }
+});
 
-// app.delete("/ap/posts/:id", async (req, res) => {
-//    try{
-//      await Post.findById(req.params.id)
-//         .then(async (post) => {
-//           const image = await gfs.find({filename: post.image}).toArray();
-//           if(image.length != 0){
-//             await gfs.delete(image[0]._id);
-//           }
-//           await Post.findByIdAndDelete(req.params.id);
-//           res.status(200).json({message: "Successfully deleted"});
-//         })
-//         .catch((err) => {
-//           console.log(`Error: ${err.message}`)
-//           res.json({message: `An error has occured.Please try again later`});
-//         });
-//    }catch(err){
-//      console.log(`Error: ${err.message}`)
-//      res.status(500).send(`We are fixing the problem right away. Please try again later`);
-//    }
-// });
+app.delete("/ap/posts/:id", async (req, res) => {
+   try{
+     await Post.findById(req.params.id)
+        .then(async (post) => {
+          const image = await gfs.find({filename: post.image}).toArray();
+          if(image.length != 0){
+            await gfs.delete(image[0]._id);
+          }
+          await Post.findByIdAndDelete(req.params.id);
+          res.status(200).json({message: "Successfully deleted"});
+        })
+        .catch((err) => {
+          console.log(`Error: ${err.message}`)
+          res.json({message: `An error has occured.Please try again later`});
+        });
+   }catch(err){
+     console.log(`Error: ${err.message}`)
+     res.status(500).send(`We are fixing the problem right away. Please try again later`);
+   }
+});
 
-// app.get("/api/isAuthenticated", (req,res) => {
+app.get("/api/isAuthenticated", (req,res) => {
  
-//     if(req.isAuthenticated()){
-//       res.json({isAuthenticated: true});
-//     } else{
-//       res.json({isAuthenticated: false});
-//     }
-// });
+    if(req.isAuthenticated()){
+      res.json({isAuthenticated: true});
+    } else{
+      res.json({isAuthenticated: false});
+    }
+});
 
-// app.get('/api', (req, res) => {
-//   res.json({ message: 'Hello from server!' });
-// });
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
 
 app.listen(port, () => {
     console.log(`API server is running on port ${port}`)
