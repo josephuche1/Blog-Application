cb0d1923058154e2e8ba0d588edc5bd0dff759a6
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-07 03:35:50 +0200

    Implemented function for mulriple file uploads

--- server/src/controllers/imageController.ts
+++ server/src/controllers/imageController.ts
@@ -1,41 +1,20 @@
 import { RequestHandler } from "express";
-import formidable from "formidable";
-import crypto from "crypto";
-import path from "path";
-import fs from "fs";
-import {getGfs} from "../utils/gridFsConfig";
 
-const gfs = getGfs();
 
 
 // create function to handle image upload
-export const uploadImage: RequestHandler = async (req, res, next) => {
-    try {
-        if (!req.files) {
-            return res.json({ success: false, message: "No files were uploaded" });
+export const imageUpload: RequestHandler = (req, res, next) => {
+    let imagesFilenames: string[] = [];
+    try{
+        if(!req.files || req.files.length === 0){
+            return res.json({msg: "No files were uploaded"});
         }
-
-        const images = Object.values(req.files) as formidable.File;
-        images.forEach(image => {
-            const filename = crypto.randomBytes(16).toString("hex") + path.extname(image.originalname);
-
-            const readStream = fs.createReadStream(image.path);
-
-            const writestream = gfs.openUploadStream(filename, {
-                chunkSizeBytes: 1048576,
-                metadata: {
-                    originalName: image.originalname,
-                    size: image.size,
-                    type: image.mimetype
-                }
-            });
-            readStream.pipe(writestream);
-            writestream.on("finish", () => {
-                console.log("File uploaded successfully");
-            });
-        });
-
-    } catch (err) {
-        res.json({ success: false, message: err.message });
+        const images = req.files as Express.Multer.File[];
+        images.forEach((image: Express.Multer.File) => {
+            imagesFilenames.push(image.filename);
+        })
+        res.json({msg: "Files uploaded successfully", imagesFilenames});
+    } catch(err){
+        next(err);
     }
-}
+};
